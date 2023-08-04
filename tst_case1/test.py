333e55cb7d57cef14ff497f6d502263fbe682d3f
Tsuyoshi Sakamoto <tsuyoshi.sakamoto@qt.io>
2023-08-04 14:01:48 +0900

    modified text compare

--- tst_case1/test.py
+++ tst_case1/test.py
@@ -5,7 +5,7 @@ import names
 
 def main():
     startApplication("testQML")
-    test.compare(str(waitForObjectExists(names.hello_World_clicked_Text).text), "clicked!")
+    test.compare(str(waitForObjectExists(names.hello_World_clicked_Text).text), "default text")
     
     mouseClick(waitForObject(names.hello_World_push_me_Button), Qt.LeftButton)
     test.compare(str(waitForObjectExists(names.hello_World_clicked_Text).text), "clicked!")
